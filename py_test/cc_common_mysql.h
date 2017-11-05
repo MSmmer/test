57db615f63740250ec898837e9d2d4cdde0e84bf
msmmer <dologengine@gmail.com>
2017-11-06 02:05:10 +0800

    Update and rename ck_common_mysql.h to cc_common_mysql.h

--- py_test/cc_common_mysql.h
+++ py_test/cc_common_mysql.h
@@ -1,5 +1,5 @@
-#ifndef CK_COMMON_MYSQL_H__
-#define CK_COMMON_MYSQL_H__
+#ifndef CC_COMMON_MYSQL_H__
+#define CC_COMMON_MYSQL_H__
 
 #include <stdio.h>
 
@@ -23,19 +23,19 @@ typedef int (*fetch_callback)(void *param, int ncols, char **row,
 * @argc    创建连接mysql数据结构
 * @return  0:成功,其他值创建失败
  */
-int ck_mysql_init(const mysql_connection_t *uri);
+int cc_mysql_init(const mysql_connection_t *uri);
 /**
 * @func    执行sql语句
 * @argc    sql语句,sql长度,执行结果
 * @return  0:成功,其他值创建失败
  */
-int ck_mysql_exec(const char *sql, size_t len, void **result);
+int cc_mysql_exec(const char *sql, size_t len, void **result);
 /**
 * @func    批量执行sql insert语句
 * @argc    sql语句,sql长度,执行结果
 * @return  0:成功,其他值创建失败
  */
-int ck_mysql_exec_batch(const char *sql, size_t len, void **outp);
+int cc_mysql_exec_batch(const char *sql, size_t len, void **outp);
 
 /**
 * @func    执行sql语句
@@ -51,28 +51,28 @@ int ck_mysql_exec_ex(const char *sql, size_t len,
 * @argc    传入字符串,字符串长度,转换后字符串
 * @return  转换后字符串长度
  */
-size_t ck_mysql_escape_string(const char *from, size_t len, char *to);
+size_t cc_mysql_escape_string(const char *from, size_t len, char *to);
 /**
 * @func    关闭连接
 * @argc
 * @return
  */
-void ck_mysql_close();
+void cc_mysql_close();
 
 /**
 * @func    读取配置文件
 * @argc    文件名
 * @return  文件内容
  */
-char *ck_mysql_read_file(const char *filename, long *size);
+char *cc_mysql_read_file(const char *filename, long *size);
 
 /**
 * @func    解析配置文件
 * @argc    mysql连接的结构
 * @return
  */
-int ck_mysql_parse_conf(mysql_connection_t *tokengen_uri);
-int ck_mysql_parse_conf_ex(mysql_connection_t *tokengen_uri);
+int cc_mysql_parse_conf(mysql_connection_t *tokengen_uri);
+int cc_mysql_parse_conf_ex(mysql_connection_t *tokengen_uri);
 #ifdef __cplusplus
 }
 #endif
